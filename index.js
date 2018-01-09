b26df15142f09e8d5ed9985f3913fdd4adaa05e0
Dipti Nayan <diptinayan21@gmail.com>
2018-01-09 17:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello,";
+var greeting = "Hello, ";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
